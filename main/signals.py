ca5f0bb9bf34e792f0264025b6103b7e37fc96e9
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-03-14 12:05:31 +0300

    balanced the background image added signals and made login dark and made more color themes

--- main/signals.py
+++ main/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from myutils import createThemes  # your utility function
 
-themeList = ["macglass", "darkglass", "lightglass"]
+themeList = ["macglass", "charcoal", "green"]
 
 @receiver(post_migrate)
 def initialize_default_themes(sender, **kwargs):
